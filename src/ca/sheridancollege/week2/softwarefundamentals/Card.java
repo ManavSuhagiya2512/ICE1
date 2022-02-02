c9c143d7e7687057410f62f46c99537ca972cc52
ManavSuhagiya <suhagiym@sheridancollege.ca>
2022-02-02 09:07:34 -0500

    Comment added

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * A class to model Card Objects.
  * Created in Week 2 for the cloning repository demo
  * @author dancye, 2019
+ * This is Inclass 1 which is guided by Ronak seth.
  */
 public class Card 
 {
